b0e7cbe12f16074cff68642aed9836924092a60f
Tom Willekes <twillekes@shaw.ca>
2012-05-19 19:04:38 -0600

    FTP support

--- src/com/twillekes/repoExporter/SiteExporter.java
+++ src/com/twillekes/repoExporter/SiteExporter.java
@@ -1,23 +1,69 @@
 package com.twillekes.repoExporter;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.SocketException;
 import java.util.Iterator;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
 
 import com.twillekes.portfolio.Picture;
 import com.twillekes.portfolio.Repository;
 
 public class SiteExporter {
+	private static final String SERVER = "ftp.shaw.ca";
 	private class FtpExporter {
 		private String password;
-		private String account;
+		private FTPClient client;
 		public FtpExporter(String password) {
 			this.password = password;
+			client = new FTPClient();
 		}
-		public void setAccount(String account) {
-			this.account = account;
+		public void initiate(String account, String destDir) throws Exception {
+			System.out.println("Initiating with account "+account);
+			client.connect(SERVER);
+			int reply = client.getReplyCode();
+			//System.out.print(client.getReplyString());
+			if (!FTPReply.isPositiveCompletion(reply)) {
+				client.disconnect();
+				throw new Exception("Server refused connection");
+			}
+			if (!client.login(account, password)) {
+				throw new Exception("Unable to log in");
+			}
+			if (!client.changeWorkingDirectory(destDir)) {
+				client.makeDirectory(destDir);
+				if (!client.changeWorkingDirectory(destDir)) {
+					throw new Exception("Could not create destination directory " + destDir);
+				}
+			}
 		}
-		public void upload(String filePath, String destFilePath) {
+		public void terminate() {
+			try {
+				client.logout();
+				client.disconnect();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		public void upload(String filePath) throws Exception {
 			// Don't forget thumbnails.
-			System.out.println("Would upload from "+filePath+" to "+destFilePath+" with account "+account+" and pw "+password);
+			String destFilePath = Picture.getFileName(filePath);
+	        String thumbPath = Picture.getThumbName(filePath);
+	        String destThumbFilePath = Picture.getFileName(thumbPath);
+
+	        System.out.println("Uploading from "+filePath+" to "+destFilePath+" and "+thumbPath+" to "+destThumbFilePath);
+			InputStream local = new FileInputStream(filePath);
+			if (!client.storeFile(destFilePath, local)) {
+				throw new Exception("Unable to upload "+filePath);
+			}
+			local = new FileInputStream(thumbPath);
+			if (!client.storeFile(destThumbFilePath, local)) {
+				throw new Exception("Unable to upload "+thumbPath);
+			}
 		}
 	}
 	public void export(String password) {
@@ -25,13 +71,33 @@ public class SiteExporter {
 		Iterator<Repository> it = Repository.get().iterator();
 		while(it.hasNext()) {
 			Repository repo = it.next();
-			String repoPath = repo.getRelativeUrl();
-			ftpExporter.setAccount(repo.getAccount());
+			String repoDir = repo.getPath();
+			try {
+				ftpExporter.initiate(repo.getAccount(), repoDir);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
 			Iterator<Picture> picIt = repo.getPictures().iterator();
 			while(picIt.hasNext()) {
 				Picture picture = picIt.next();
-				ftpExporter.upload(picture.getRepositoryFilePath(), repoPath + Picture.getFileName(picture.getFilePath()));
+				try {
+					ftpExporter.upload(picture.getRepositoryFilePath());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
+			ftpExporter.terminate();
+		}
+	}
+	public void test(String pw) {
+		FtpExporter ftpExporter = new FtpExporter(pw);
+		try {
+			ftpExporter.initiate("photonwrangler", "testUpload/");
+			ftpExporter.upload("repository/TESTIMAGE.jpg");
+			ftpExporter.terminate();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
--- src/com/twillekes/userInteraction/PublishRepository.java
+++ src/com/twillekes/userInteraction/PublishRepository.java
@@ -1,15 +1,48 @@
 package com.twillekes.userInteraction;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 import com.twillekes.repoExporter.SiteExporter;
+import com.twillekes.userInterface.Application;
 
 public class PublishRepository implements SelectionListener {
+	String pass;
 	@Override
 	public void widgetSelected(SelectionEvent e) {
-		SiteExporter siteExporter = new SiteExporter();
-		siteExporter.export("testpw");
+		Shell shell = new Shell(Application.getShell(), SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+		shell.setLayout(new GridLayout());
+	    (new Label(shell, SWT.NULL)).setText("Password: ");
+	    Text textPassword = new Text(shell, SWT.SINGLE | SWT.BORDER | SWT.PASSWORD);
+	    textPassword.setEchoChar('*');
+	    textPassword.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				pass = ((Text)e.widget).getText();
+			}
+	    });
+	    textPassword.addDisposeListener(new DisposeListener(){
+			@Override
+			public void widgetDisposed(DisposeEvent e) {
+				SiteExporter siteExporter = new SiteExporter();
+//				siteExporter.export("testpw");
+				siteExporter.test(pass);
+			}
+	    });
+		shell.layout();
+		shell.pack();
+		shell.open();
 	}
 	@Override
 	public void widgetDefaultSelected(SelectionEvent e) {
